399b2997b1440ba86eba8e5b83ea30db85e4eab3
Marco Duarte <mduarte@pn1808074.lanl.gov>
2018-07-25 08:59:19 -0600

    First commit

---
+++ boardState.h
@@ -0,0 +1,15 @@
+/*
+ * boardState.h
+ *
+ *  Created on: Jul 25, 2018
+ *      Author: mduarte
+ */
+
+#ifndef BOARDSTATE_H_
+#define BOARDSTATE_H_
+
+
+
+
+
+#endif /* BOARDSTATE_H_ */
---
+++ game.h
@@ -0,0 +1,15 @@
+/*
+ * game.h
+ *
+ *  Created on: Jul 25, 2018
+ *      Author: mduarte
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+
+
+
+
+#endif /* GAME_H_ */
